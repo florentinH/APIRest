25f3dd766aa0e1adabef6b1a7c7acbbf7f222fc1
florentinH <flokri14@gmail.com>
2018-09-05 10:18:44 +0200

    front and back communicated

--- test-stack-labs/back/server.js
+++ test-stack-labs/back/server.js
@@ -1,8 +1,9 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+const app = express()
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(5000)

55f5832154bd1d9c9a80981381a3275d71de40b2
florentinH <flokri14@gmail.com>
2018-09-12 18:11:20 +0200

    adding invention valid

--- test-stack-labs/back/server.js
+++ test-stack-labs/back/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const inventionsRouter = require ('./routes/inventions')
+
+app.use(bodyParser.json())
 
 
 app.use('/api/inventions', inventionsRouter)
--- test-stack-labs/src/App.js
+++ test-stack-labs/src/App.js
@@ -6,13 +6,36 @@ import './InventionsList.css'
 
 class App extends Component {
   state = {
-    inventions: []
+    inventions: [],
+    addInvention: []
   }
 
   componentDidMount() {
     fetch('/api/inventions')
     .then(res => res.json())
     .then(inventions => this.setState({inventions: inventions}))
+  }
+
+  handleChange = e => {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    fetch('/api/inventions', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(this.state)
+    })
+    .then(res => res.json())
+    .then(newInvention => {
+      console.log(newInvention)
+    })
   }
 
 
@@ -22,6 +45,22 @@ class App extends Component {
         <header className="List-header">
           <h1 className="List-title">Liste des Inventions</h1>
         </header>
+
+        <div className="List-list">
+          <form onSubmit={this.handleSubmit}>
+            <h5>Ajouter une invention</h5>
+            <div>
+              <input name="name" placeholder="Nom de l'invention" onChange={this.handleChange}/>
+              <input name="date" placeholder="Année" onChange={this.handleChange}/>
+              <input name="inventor" placeholder="Nom de l'inventeur" onChange={this.handleChange}/>
+              <input name="origin" placeholder="Pays" onChange={this.handleChange}/>
+              <input name="site" placeholder="Site internet" onChange={this.handleChange}/>
+              <button type="submit">
+                <span className="icon-checkmark">Valider</span>
+              </button>
+            </div>          
+          </form>
+        </div>
 
           <div className="List-list">
             <Invention inventionList={this.state.inventions} />

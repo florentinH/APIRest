c1b8fb8c5e05ad442f28e64c02c36169d7eb4c8d
florentinH <flokri14@gmail.com>
2018-09-12 12:04:24 +0200

    add db

--- test-stack-labs/back/routes/inventions.js
+++ test-stack-labs/back/routes/inventions.js
@@ -13,4 +13,24 @@ router.get('/', (req, res)=> {
     })
 })
 
+router.post('/', (req, res) => {
+    const {date, name, inventor, origin, site} = req.body
+    const query = 'INSERT INTO inventions (date, name, inventor, origin, site) VALUES (?, ?, ?, ?, ?)'
+    const selectQuery = 'SELECT * from inventions WHERE id = ?'
+    db.query(query, [date, name, inventor, origin, site], (err, result) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).json({error: 'Database query failed'})
+      }
+      db.query(selectQuery, [result.insertId], (err, inventions) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).json({error: 'Database query failed'})
+        }
+  
+        res.json(inventions[0])
+      })
+    })
+  })
+
 module.exports = router
